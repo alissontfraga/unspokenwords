dece09dc9b75a2c4459918690f0394ce3d6131da
alissontfraga <alissontfraga@gmail.com>
2025-12-16 14:01:59 -0300

    feat: add User entity

---
+++ backend/src/main/java/com/alissontfraga/unspokenwords/model/User.java
@@ -0,0 +1,31 @@
+package com.alissontfraga.unspokenwords.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, length = 30)
+    private String username;
+    @Column(nullable = false, length = 60)  
+    private String password;
+    
+}
