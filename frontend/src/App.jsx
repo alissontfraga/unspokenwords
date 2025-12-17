d40987a23c98d0258d01e9053e391e803e6ad930
alissontfraga <alissontfraga@gmail.com>
2025-12-17 10:29:33 -0300

    feat: implement main application structure with routing and authentication context

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Messages from "./pages/Messages";
+import NewMessage from "./pages/NewMessage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { Toaster } from "react-hot-toast";
+import Footer from  "./components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Toaster position="top-right" />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/messages" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/messages"
+          element={
+            <ProtectedRoute>
+              <Messages />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/messages/new"
+          element={
+            <ProtectedRoute>
+              <NewMessage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
+}
